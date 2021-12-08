1bb3f60da20a7b77ed6837950f2c800595d1f536
jpclark6 <jpclark6@gmail.com>
2021-12-08 08:18:07 -0700

    refactored day 6

--- 6_day/main.py
+++ 6_day/main.py
@@ -1,6 +1,3 @@
-from copy import copy
-
-
 lanternfish = {x: 0 for x in range(9)}
 
 with open('puzzle.py', 'r') as f:
@@ -11,20 +8,9 @@ def age_fish(lanternfish, days):
     for _ in range(days):
         new_lanternfish = {x: 0 for x in range(9)}
         for f in range(9):
-            new_lanternfish[8 - f] = lanternfish[f]
-        new_lanternfish[6] = new_lanternfish[6] + lanternfish[7] + lanternfish[0]
-        
-        # new_lanternfish[8] = lanternfish[0]
-        # new_lanternfish[7] = lanternfish[8]
-        new_lanternfish[6] = lanternfish[7] + lanternfish[0]
-        # new_lanternfish[5] = lanternfish[6]
-        # new_lanternfish[4] = lanternfish[5]
-        # new_lanternfish[3] = lanternfish[4]
-        # new_lanternfish[2] = lanternfish[3]
-        # new_lanternfish[1] = lanternfish[2]
-        # new_lanternfish[0] = lanternfish[1]
+            new_lanternfish[8 - f] = lanternfish[(9 - f) % 9]
+        new_lanternfish[6] = new_lanternfish[6] + lanternfish[0]
         lanternfish = new_lanternfish
-
     return sum(lanternfish.values())
 
 part_1 = age_fish(lanternfish, 80)
